abdd9cbedc78f7ca0aa4b65682c9695004e7561f
Sanjay Dey <deysanjay285@gmail.com>
2024-12-10 12:52:45 -0500

    initial project setup completeted

---
+++ WhiteboxCryptography/WhiteboxCryptography.h
@@ -0,0 +1,18 @@
+//
+//  WhiteboxCryptography.h
+//  WhiteboxCryptography
+//
+//  Created by Sanjay Dey on 2024-12-10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WhiteboxCryptography.
+FOUNDATION_EXPORT double WhiteboxCryptographyVersionNumber;
+
+//! Project version string for WhiteboxCryptography.
+FOUNDATION_EXPORT const unsigned char WhiteboxCryptographyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WhiteboxCryptography/PublicHeader.h>
+
+
---
+++ WhiteboxCryptographyTests/WhiteboxCryptographyTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteboxCryptographyTests.swift
+//  WhiteboxCryptographyTests
+//
+//  Created by Sanjay Dey on 2024-12-10.
+//
+
+import Testing
+@testable import WhiteboxCryptography
+
+struct WhiteboxCryptographyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
