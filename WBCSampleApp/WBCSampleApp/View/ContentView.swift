d31a1c06415bd10c17333369e686f635f9a45c29
Sanjay Dey <deysanjay285@gmail.com>
2024-12-14 20:42:16 -0500

    code reafactored

--- WBCSampleApp/WBCSampleApp/View/ContentView.swift
+++ WBCSampleApp/WBCSampleApp/View/ContentView.swift
@@ -12,146 +12,197 @@ import WhiteboxCryptography
 struct ContentView: View {
     @StateObject private var viewModel = ContentViewModel()
     @FocusState private var isTextFieldFocused: Bool
+    @State private var showErrorAlert = false
+    @State private var errorMessage = ""
 
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 16) {
-                // Text field for input text
+                // Input Text Field
                 TextField("Enter text to encrypt", text: $viewModel.inputText)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
-                    .focused($isTextFieldFocused) // Bind the focus state
+                    .focused($isTextFieldFocused)
 
-                // Picker for selecting encryption algorithm
-                HStack(alignment: .center) {
-                    Text("Select an encryption algorithm:")
-                        .font(.headline) // Descriptive text for the picker
-                        .padding(.bottom, 4) // Adds space between text and picker
+                // Algorithm Picker
+                AlgorithmPickerView(viewModel: viewModel)
 
-                    Picker("Algorithm", selection: $viewModel.selectedAlgorithm) {
-                        ForEach(viewModel.algorithms, id: \.self) { algorithm in
-                            Text(viewModel.selectedAlgorithmDescription(for: algorithm))
-                                .tag(algorithm)
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-                    .onChange(of: viewModel.selectedAlgorithm) { oldAlgorithm, newAlgorithm in
-                        // When algorithm changes, update the valid key sizes
-                        viewModel.selectedAlgorithm = newAlgorithm
-                    }
-                }
+                // Key Size Picker
+                KeySizePickerView(viewModel: viewModel)
 
-                // Key Size Picker for selected algorithm
-                HStack(alignment: .center) {
-                    Text("Select key size:")
-                        .font(.headline) // Descriptive text for the key size picker
-                        .padding(.bottom, 4) // Adds space between text and picker
-
-                    let validKeySizes = viewModel.selectedAlgorithm.validKeySizes
-                    switch validKeySizes.first {
-                    case .specific:
-                        // For specific key sizes, use a HStack for the Picker
-                        Picker("Select Key Size", selection: $viewModel.selectedKeySize) {
-                            ForEach(viewModel.keySizes, id: \.self) { size in
-                                Text("\(size) bits")
-                                    .tag(size)
-                            }
-                        }
-                        .pickerStyle(MenuPickerStyle())
-                        
-                    case .range(let min, let max):
-                        // For range key sizes, conditionally use VStack inside the HStack
-                        VStack(alignment: .leading, spacing: 10) {
-                            Text("Key Size: \(viewModel.selectedKeySize) bits")
-                                .padding(.bottom, 4)
-
-                            Slider(value: Binding(get: {
-                                Double(viewModel.selectedKeySize)
-                            }, set: { newValue in
-                                viewModel.selectedKeySize = Int(newValue)
-                            }), in: Double(min)...Double(max), step: 1)
-                                .onChange(of: viewModel.selectedKeySize) { _, newKeySize in
-                                    viewModel.updateAlgorithmWithKeySize(updatedkeysize: newKeySize)
-                                }
-
-                            Text("Range: \(min) bits to \(max) bits")
-                                .font(.subheadline)
-                                .foregroundColor(.gray)
-                        }
-
-                    default:
-                        EmptyView()
-                    }
-                }
-
-
-                // AES Mode Picker if AES is selected
+                // AES Mode Picker (only for AES)
                 if case .aes = viewModel.selectedAlgorithm {
-                    HStack(alignment: .center) {
-                        Text("Select AES mode:")
-                            .font(.headline) // Descriptive text for AES mode picker
-                            .padding(.bottom, 4) // Adds space between text and picker
-
-                        Picker("Mode", selection: $viewModel.aesMode) {
-                            Text("ECB").tag(AESMode.ecb)
-                            Text("CBC").tag(AESMode.cbc)
-                            Text("GCM").tag(AESMode.gcm)
-                        }
-                        .pickerStyle(MenuPickerStyle()) // Dropdown style
-                        .onChange(of: viewModel.aesMode) { _, newMode in
-                            viewModel.selectedAlgorithm = .aes(keySize: viewModel.selectedKeySize, mode: newMode, processingType: viewModel.selectedAlgorithm.processingType)
-                        }
-                    }
+                    AesModePickerView(viewModel: viewModel)
                 }
 
-                // Processing type picker
-                HStack(alignment: .center) {
-                    Text("Select processing type:")
-                        .font(.headline) // Descriptive text for processing type picker
-                        .padding(.bottom, 4) // Adds space between text and picker
+                // Processing Type Picker
+                ProcessingTypePickerView(viewModel: viewModel)
 
-                    Picker("Processing Type", selection: $viewModel.aesProcessingType) {
-                        Text("Faster").tag(ProcressingType.faster)
-                        Text("Regular").tag(ProcressingType.regular)
-                    }
-                    .pickerStyle(MenuPickerStyle()) // Dropdown style
-                    .onChange(of: viewModel.aesProcessingType) { _, newProcessingType in
-                        viewModel.updateProccessingType(processingType: newProcessingType)
-                    }
-                }
-
-                // Button to trigger both encryption and decryption
+                // Encrypt & Decrypt Button
                 Button("Encrypt & Decrypt") {
-                    viewModel.encryptAndDecrypt()
+                    encryptAndDecrypt()
                 }
-                .frame( alignment: .center)
+                .frame(maxWidth: .infinity)
                 .buttonStyle(.borderedProminent)
+                .padding()
 
-                // Display the encrypted text if available
-                if !viewModel.encryptedText.isEmpty {
-                    Text("Encrypted Text: \(viewModel.encryptedText)")
-                        .padding()
-                }
-
-                // Display the decrypted text if available
-                if !viewModel.decryptedText.isEmpty {
-                    Text("Decrypted Text: \(viewModel.decryptedText)")
-                        .padding()
-                }
+                // Display Results
+                EncryptedDecryptedTextView(viewModel: viewModel)
 
                 Spacer()
             }
             .padding()
             .onTapGesture {
-                // Dismiss the keyboard on tap
                 isTextFieldFocused = false
             }
+            .alert(isPresented: $showErrorAlert) {
+                Alert(title: Text("Error"), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
+        }
+    }
+    
+    // Handle encryption and decryption actions
+    private func encryptAndDecrypt() {
+        viewModel.encryptAndDecrypt()
+    }
+}
 
+// Algorithm Picker View
+struct AlgorithmPickerView: View {
+    @ObservedObject var viewModel: ContentViewModel
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Select an encryption algorithm:")
+                .font(.headline)
+
+            Picker("Algorithm", selection: $viewModel.selectedAlgorithm) {
+                ForEach(viewModel.algorithms, id: \.self) { algorithm in
+                    Text(viewModel.selectedAlgorithmDescription(for: algorithm))
+                        .tag(algorithm)
+                }
+            }
+            .pickerStyle(MenuPickerStyle())
+            .onChange(of: viewModel.selectedAlgorithm) { _,_ in
+                viewModel.updateKeySizes()
+            }
+        }
+        .padding(.bottom, 8)
+    }
+}
+
+// Key Size Picker View
+struct KeySizePickerView: View {
+    @ObservedObject var viewModel: ContentViewModel
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Select key size:")
+                .font(.headline)
+
+            let validKeySizes = viewModel.selectedAlgorithm.validKeySizes
+            if let firstKeySize = validKeySizes.first {
+                switch firstKeySize {
+                case .specific:
+                    Picker("Select Key Size", selection: $viewModel.selectedKeySize) {
+                        ForEach(viewModel.keySizes, id: \.self) { size in
+                            Text("\(size) bits")
+                                .tag(size)
+                        }
+                    }
+                    .pickerStyle(MenuPickerStyle())
+                    
+                case .range(let min, let max):
+                    VStack {
+                        Text("Key Size: \(viewModel.selectedKeySize) bits")
+                        Slider(value: Binding(get: {
+                            Double(viewModel.selectedKeySize)
+                        }, set: { newValue in
+                            viewModel.selectedKeySize = Int(newValue)
+                        }), in: Double(min)...Double(max), step: 1)
+                        .onChange(of: viewModel.selectedKeySize) { _,newKeySize in
+                            viewModel.updateAlgorithmWithKeySize(updatedKeySize: newKeySize)
+                        }
+                        Text("Range: \(min) bits to \(max) bits")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+        }
+        .padding(.bottom, 8)
+    }
+}
+
+// AES Mode Picker View
+struct AesModePickerView: View {
+    @ObservedObject var viewModel: ContentViewModel
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Select AES mode:")
+                .font(.headline)
+
+            Picker("Mode", selection: $viewModel.aesMode) {
+                Text("ECB").tag(AESMode.ecb)
+                Text("CBC").tag(AESMode.cbc)
+                Text("GCM").tag(AESMode.gcm)
+            }
+            .pickerStyle(MenuPickerStyle())
+            .onChange(of: viewModel.aesMode) { _,newMode in
+                viewModel.selectedAlgorithm = .aes(
+                    keySize: viewModel.selectedKeySize,
+                    mode: newMode,
+                    processingType: viewModel.selectedAlgorithm.processingType
+                )
+            }
+        }
+        .padding(.bottom, 8)
+    }
+}
+
+// Processing Type Picker View
+struct ProcessingTypePickerView: View {
+    @ObservedObject var viewModel: ContentViewModel
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Select processing type:")
+                .font(.headline)
+
+            Picker("Processing Type", selection: $viewModel.aesProcessingType) {
+                Text("Faster").tag(ProcressingType.faster)
+                Text("Regular").tag(ProcressingType.regular)
+            }
+            .pickerStyle(MenuPickerStyle())
+            .onChange(of: viewModel.aesProcessingType) { _,newProcessingType in
+                viewModel.updateProcessingType(processingType: newProcessingType)
+            }
+        }
+        .padding(.bottom, 8)
+    }
+}
+
+// Encrypted and Decrypted Text View
+struct EncryptedDecryptedTextView: View {
+    @ObservedObject var viewModel: ContentViewModel
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            if !viewModel.encryptedText.isEmpty {
+                Text("Encrypted Text: \(viewModel.encryptedText)")
+                    .padding()
+            }
+
+            if !viewModel.decryptedText.isEmpty {
+                Text("Decrypted Text: \(viewModel.decryptedText)")
+                    .padding()
+            }
         }
     }
 }
 
-// Preview for the ContentView
+// Preview
 #Preview {
     ContentView()
 }
--- WBCSampleApp/WBCSampleApp/ViewModel/ContentViewModel.swift
+++ WBCSampleApp/WBCSampleApp/ViewModel/ContentViewModel.swift
@@ -6,7 +6,7 @@
 //
 
 
-import SwiftUI
+import Foundation
 import Combine
 import WhiteboxCryptography
 
@@ -14,218 +14,178 @@ class ContentViewModel: ObservableObject {
     @Published var inputText: String = ""
     @Published var encryptedText: String = ""
     @Published var decryptedText: String = ""
-    @Published var selectedAlgorithm: CryptoAlgorithm = CryptoAlgorithm.aes(keySize: 256, mode: .ecb, processingType: .regular){
+    @Published var selectedAlgorithm: CryptoAlgorithm = .aes(keySize: 256, mode: .ecb, processingType: .regular) {
         didSet {
-            updateKeySizes() // Update key sizes when algorithm changes
+            updateKeySizes()
         }
     }
-   
-    //@Published var aesKeySize: AESKeySize = .bits128
+    
     @Published var aesMode: AESMode = .ecb
     @Published var aesProcessingType: ProcressingType = .regular
-    @Published var validKeySizes: [CryptoKeySize] = [] // Added to track valid key sizes
+    @Published var validKeySizes: [CryptoKeySize] = [] // Track valid key sizes
     @Published var selectedKeySize: Int = 256  // Default to 256 bits
-    @Published var keySizes: [Int] = [] // Published property to bind to the UI
+    @Published var keySizes: [Int] = [] // Key size options
 
-    private var key:Data = Data()
+    private var key: Data = Data()
     private var iv: Data = Data()
-    private var memoryKey = "defaultMemoryKey".data(using: .utf8)!
     private let sdk: WhiteboxCryptographySDK
     private var cancellables = Set<AnyCancellable>()
+
     @Published var algorithms: [CryptoAlgorithm] = [
-            .aes(keySize: 128, mode: .ecb, processingType: .regular),
-            .des(keySize: 64, processingType: .regular),
-            .tripleDES(keySize: 192, processingType: .regular),
-            .cast(keySize: 40, processingType: .regular),
-            .rc2(keySize: 756, processingType: .regular)
-        ]
+        .aes(keySize: 128, mode: .ecb, processingType: .regular),
+        .des(keySize: 64, processingType: .regular),
+        .tripleDES(keySize: 192, processingType: .regular),
+        .cast(keySize: 40, processingType: .regular),
+        .rc2(keySize: 756, processingType: .regular)
+    ]
 
-    private var isEncrypted: Bool = false // Track if encryption was successful
+    private var isEncrypted: Bool = false
 
-    init() {
-        self.sdk = WhiteboxCryptographySDK(memoryKey: memoryKey)
+    init(sdk: WhiteboxCryptographySDK = WhiteboxCryptographySDK(memoryKey: "defaultMemoryKey".data(using: .utf8)!)) {
+        self.sdk = sdk
         updateKeySizes()
     }
 
-    private func updateKeySizes() {
+    // MARK: - Algorithm Management
+    func updateKeySizes() {
         var sizes: [Int] = []
         
-        // Loop through each valid key size and handle both specific and range cases
+        // Add key sizes based on selected algorithm
         for keySize in selectedAlgorithm.validKeySizes {
             switch keySize {
             case .specific(let size):
                 sizes.append(size)
-                
             case .range:
                 return
             }
         }
-       
+        
         keySizes = sizes
-        selectedKeySize = keySizes[0]
-       
+        selectedKeySize = keySizes.first ?? 256
     }
-    
-    
+
     func selectedAlgorithmDescription() -> String {
-            switch selectedAlgorithm {
-            case .aes:
-                return "AES"
-            case .des:
-                return "DES"
-            case .tripleDES:
-                return "Triple DES"
-            case .cast:
-                return "CAST"
-            case .rc2:
-                return "RC2"
-            }
-        }
-    
+        return selectedAlgorithm.description
+    }
+
     func selectedAlgorithmDescription(for algorithm: CryptoAlgorithm) -> String {
-        switch algorithm {
-        case .aes:
-            return "AES"
-        case .des:
-            return "DES"
-        case .tripleDES:
-            return "Triple DES"
-        case .cast:
-            return "CAST"
-        case .rc2:
-            return "RC2"
-        }
+        return algorithm.description
     }
 
-
-    // Method to encrypt and decrypt text
+    // MARK: - Encryption and Decryption
     func encryptAndDecrypt() {
-        // Step 1: Encrypt text
-        
-        print("selectedAlgorithm \(selectedAlgorithm)")
-        if let _ = encryptText() {
-            decryptText()
+        guard let encrypted = encryptText() else {
+            print("Encryption failed.")
+            return
         }
-
-        
+        decryptText(with: encrypted)
     }
 
-    private func encryptText() -> String?{
-        do {
-            guard !inputText.isEmpty else {
-                print("Input text is empty, cannot encrypt.")
-                encryptedText = ""
-                decryptedText = ""
-                isEncrypted = false
-                return nil
-            }
+    private func encryptText() -> String? {
+        guard !inputText.isEmpty else {
+            print("Input text is empty, cannot encrypt.")
+            resetEncryptedAndDecryptedText()
+            return nil
+        }
 
+        do {
             let currentAlgorithm = selectedAlgorithm
 
-            // Ensure the IV is created based on the selected algorithm
+            // Generate IV based on the selected algorithm
             guard let ivData = sdk.generateRandomIV(forAlgorithm: currentAlgorithm) else {
-                print("Error generating IV")
-                encryptedText = ""
-                decryptedText = ""
-                isEncrypted = false
+                print("Error generating IV.")
+                resetEncryptedAndDecryptedText()
                 return nil
             }
 
             self.iv = ivData
+            self.key = getKeyData(forSize: selectedKeySize)
 
-            // Determine the key size based on the selected AES key size
-            self.key = getKeyData(forSize: selectedAlgorithm.keySize)
-
-            if let encryptedData = try sdk.encrypt(
+            // Perform encryption
+            guard let encryptedData = try sdk.encrypt(
                 data: inputText.data(using: .utf8)!,
                 withKey: key,
                 iv: iv,
                 algorithm: currentAlgorithm
-            ) {
-                encryptedText = encryptedData.base64EncodedString()
-                print("Encryption successful: \(encryptedText)")
-                isEncrypted = true // Mark as successful
-                return encryptedText
-            } else {
-                encryptedText = ""
-                decryptedText = ""
-                isEncrypted = false
+            ) else {
+                print("Encryption failed.")
+                resetEncryptedAndDecryptedText()
+                return nil
             }
+
+            encryptedText = encryptedData.base64EncodedString()
+            isEncrypted = true
+            print("Encryption successful: \(encryptedText)")
+            return encryptedText
         } catch {
             print("Encryption error: \(error)")
-            encryptedText = ""
-            decryptedText = ""
-            isEncrypted = false
+            resetEncryptedAndDecryptedText()
             return nil
         }
-        return nil
     }
 
-    private func decryptText() {
-        do {
-            guard let encryptedData = Data(base64Encoded: self.encryptedText) else {
-                print("Invalid base64 encoded string.")
-                decryptedText = "Decryption failed"
-                return
-            }
+    private func decryptText(with encryptedText: String) {
+        guard let encryptedData = Data(base64Encoded: encryptedText) else {
+            print("Invalid base64 encoded string.")
+            decryptedText = "Decryption failed."
+            return
+        }
 
+        do {
             let currentAlgorithm = selectedAlgorithm
 
-            // Decrypt with the same key and IV used for encryption
-
-            if let decryptedData = try sdk.decrypt(
+            // Perform decryption
+            guard let decryptedData = try sdk.decrypt(
                 data: encryptedData,
                 withKey: key,
                 iv: iv,
                 algorithm: currentAlgorithm
-            ) {
-                decryptedText = String(data: decryptedData, encoding: .utf8) ?? "Decryption failed"
-                print("Decryption successful: \(decryptedText)")
-            } else {
-                decryptedText = "Decryption failed"
+            ) else {
+                decryptedText = "Decryption failed."
+                return
             }
+
+            decryptedText = String(data: decryptedData, encoding: .utf8) ?? "Decryption failed."
+            print("Decryption successful: \(decryptedText)")
         } catch {
             print("Decryption error: \(error)")
-            decryptedText = "Decryption failed"
+            decryptedText = "Decryption failed."
         }
     }
 
-    // Helper function to determine key data for the selected AES key size
+    // MARK: - Key Management
     private func getKeyData(forSize sizeInBits: Int) -> Data {
-        let sizeInBytes = sizeInBits / 8 // Convert bits to bytes
+        let sizeInBytes = sizeInBits / 8
         var keyData = Data(count: sizeInBytes)
-        
-        // Generate random bytes and fill the Data object
+
         let result = keyData.withUnsafeMutableBytes { bytes in
             SecRandomCopyBytes(kSecRandomDefault, sizeInBytes, bytes.baseAddress!)
         }
-        
-        // Check for errors and return the keyData
-        if result == errSecSuccess {
-            return keyData
-        } else {
-            // Handle the error (you can return an empty Data or handle it differently)
-            fatalError("Failed to generate random key data")
+
+        guard result == errSecSuccess else {
+            fatalError("Failed to generate random key data.")
         }
+
+        return keyData
     }
-    
-    func updateAlgorithmWithKeySize(updatedkeysize: Int) {
-        self.selectedKeySize = updatedkeysize
+
+    func updateAlgorithmWithKeySize(updatedKeySize: Int) {
+        self.selectedKeySize = updatedKeySize
         switch selectedAlgorithm {
         case .aes(_, let mode, let processingType):
-            selectedAlgorithm = .aes(keySize: updatedkeysize, mode: mode, processingType: processingType)
+            selectedAlgorithm = .aes(keySize: updatedKeySize, mode: mode, processingType: processingType)
         case .des(_, let processingType):
-            selectedAlgorithm = .des(keySize: updatedkeysize, processingType: processingType)
+            selectedAlgorithm = .des(keySize: updatedKeySize, processingType: processingType)
         case .tripleDES(_, let processingType):
-            selectedAlgorithm = .tripleDES(keySize: updatedkeysize, processingType: processingType)
+            selectedAlgorithm = .tripleDES(keySize: updatedKeySize, processingType: processingType)
         case .cast(_, let processingType):
-            selectedAlgorithm  = .cast(keySize: updatedkeysize, processingType: processingType)
+            selectedAlgorithm = .cast(keySize: updatedKeySize, processingType: processingType)
         case .rc2(_, let processingType):
-            selectedAlgorithm = .rc2(keySize: updatedkeysize, processingType: processingType)
+            selectedAlgorithm = .rc2(keySize: updatedKeySize, processingType: processingType)
         }
     }
-    
-    func updateProccessingType(processingType: ProcressingType) {
+
+    func updateProcessingType(processingType: ProcressingType) {
         self.aesProcessingType = processingType
         switch selectedAlgorithm {
         case .aes:
@@ -238,9 +198,30 @@ class ContentViewModel: ObservableObject {
             selectedAlgorithm = .cast(keySize: selectedAlgorithm.keySize, processingType: processingType)
         case .rc2:
             selectedAlgorithm = .rc2(keySize: selectedAlgorithm.keySize, processingType: processingType)
-
         }
-        
     }
 
+    // MARK: - Helper Methods
+    private func resetEncryptedAndDecryptedText() {
+        encryptedText = ""
+        decryptedText = ""
+        isEncrypted = false
+    }
+}
+
+extension CryptoAlgorithm {
+    var description: String {
+        switch self {
+        case .aes:
+            return "AES"
+        case .des:
+            return "DES"
+        case .tripleDES:
+            return "Triple DES"
+        case .cast:
+            return "CAST"
+        case .rc2:
+            return "RC2"
+        }
+    }
 }
